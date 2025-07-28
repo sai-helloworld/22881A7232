4bf4c97cb0345fd412984cee90298e752821777d
Sai Pavan <138319108+sai-helloworld@users.noreply.github.com>
2025-07-28 13:25:42 +0530

    Add files via upload

---
+++ BackEndTestSubmission/logger/logconfig.py
@@ -0,0 +1,24 @@
+import logging
+from logging.config import dictConfig
+
+def setup_logging():
+    logging_config = {
+        "version": 1,
+        "formatters": {
+            "default": {
+                "format": "%(asctime)s - %(levelname)s - %(message)s",
+            },
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "formatter": "default",
+            },
+        },
+        "root": {
+            "level": "INFO",
+            "handlers": ["console"],
+        },
+    }
+
+    dictConfig(logging_config)
